d2dadcd4223d0e3cd29183c9cf9f4b2cb67d02ef
jonedo03 <jonedo03@luther.edu>
2015-10-05 09:39:46 -0700

    Create todo3.css

---
+++ todo3.css
@@ -0,0 +1,35 @@
+.high {background-color:red;
+height: 200px;
+  width: 300px;
+size:12pt;
+}
+.medium {background-color:orange;
+height: 200px;
+  width: 300px;
+size:12pt;
+}
+.low {background-color:gray;
+height: 200px;
+  width: 300px;
+size:12pt;
+}
+.done {background-color:black;
+height: 200px;
+  width: 300px;
+size:12pt;
+}
+.high li {color:white;
+}
+.medium li {color:white;
+}
+.low li {color:black;
+}
+.done li {color:cyan;
+}
+section {
+  border: 5px solid black;
+  height: 200px;
+  width: 300px;
+  overflow: auto;
+  float: right;
+}
