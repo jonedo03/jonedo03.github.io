f5ec2076240f85736ca97f6d40b5eeb6b3701064
jonedo03 <jonedo03@luther.edu>
2015-10-21 11:01:01 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,25 @@
+addli = Function() {
+  var myinput1, mylist value, dropdown;
+  myinput1 = document.querySelector ('#addli');
+  value1 = document.createTextnode(myinput1.value)
+  mylist.document.querySelector("#task")
+  document.ul.appendChild(myul)
+  // dropdowninput = document.querySelector("#priority");
+  // dropdowninput.value
+  
+  
+  
+
+  input = document.createElement("input")
+  input.type = "checkbox"
+  item = document.createElement("li")
+  item.appendChild(input);
+  item.appendChild(value);
+  mylist.appendChild(item);
+}
+  
+  checkfunc
+}
+  
+  
 
