c563fd624163e453e4d55df58712a812853772c0
jonedo03 <jonedo03@luther.edu>
2015-10-23 07:51:19 -0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,25 +1,36 @@
 addli = Function() {
-  var myinput1, mylist value, dropdown;
-  myinput1 = document.querySelector ('#addli');
-  value1 = document.createTextnode(myinput1.value)
-  mylist.document.querySelector("#task")
-  document.ul.appendChild(myul)
+  var myBox, mylist,myLi, myPriority, mytext, myUL, myInput;
+  
+  myBox = document.querySelector ('#textbox');
+  
+  myPriority = document.querySelector("#priority");
+  
+  myLi = document.createElement("li");
+  
+  myLi.classlist.add(myPriority.value);
+  
+  myText = document.createTextNode(myBox.value);
+  
+ myLi.appendChild(myText);
+ 
+ myUL = document.querySelector("#tasklist");
+ 
+ myUL.appendChild(myLi);
+ 
+ myInput = document.createElement("INPUT");
+ 
+ myInput.type = "checkbox";
+ 
+ myLi.appendChild(myInput);
+ 
+}
+  
   // dropdowninput = document.querySelector("#priority");
   // dropdowninput.value
   
   
   
 
-  input = document.createElement("input")
-  input.type = "checkbox"
-  item = document.createElement("li")
-  item.appendChild(input);
-  item.appendChild(value);
-  mylist.appendChild(item);
-}
-  
-  checkfunc
-}
-  
+ 
   
 
